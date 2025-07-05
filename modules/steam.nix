e220c5dcae6056a23e1750c986042005f8c36169
crs553 <42813301+crs553@users.noreply.github.com>
2025-07-05 19:31:58 +0100

    feat: steam added

---
+++ modules/steam.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, unstablePkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.devtools;
+in {
+  options.devtools = {
+    enableSteam = mkEnableOption "Enable Steam";
+  };
+
+  config = mkIf cfg.enableSteam {
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
+  };
+}
+
