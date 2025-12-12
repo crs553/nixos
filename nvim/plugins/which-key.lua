16ca428ff882b7c0574981f19b4fe990b9d81aac
CS <42813301+crs553@users.noreply.github.com>
2025-12-12 06:51:58 +0000

    feat: which key map

--- nvim/plugins/which-key.lua
+++ nvim/plugins/which-key.lua
@@ -4,3 +4,12 @@ local wk = require("which-key")
 
 wk.setup({
 })
+wk.add({
+  {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+  },
+})
