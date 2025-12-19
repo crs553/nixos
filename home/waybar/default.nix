00f3a3cb25c2d644cf422fe53d48479c0dde0b1e
CS <42813301+crs553@users.noreply.github.com>
2025-12-19 11:43:47 +0000

    fix: waybar styling

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -12,7 +12,7 @@
       {
         position = "top";
         height = 35;
-        spacing = 0;
+        spacing = 4;
 
         modules-left = [
           "hyprland/workspaces"
@@ -134,76 +134,196 @@
     ];
 
     style = ''
-      /* ===== Catppuccin Colors ===== */
-      @define-color rosewater #f5e0dc;
-      @define-color flamingo #f2cdcd;
-      @define-color pink #f5c2e7;
-      @define-color mauve #cba6f7;
-      @define-color red #f38ba8;
-      @define-color maroon #eba0ac;
-      @define-color peach #fab387;
-      @define-color yellow #f9e2af;
-      @define-color green #a6e3a1;
-      @define-color teal #94e2d5;
-      @define-color sky #89dceb;
-      @define-color sapphire #74c7ec;
-      @define-color blue #89b4fa;
-      @define-color lavender #b4befe;
-      @define-color text #cdd6f4;
-      @define-color subtext1 #bac2de;
-      @define-color subtext0 #a6adc8;
-      @define-color overlay2 #9399b2;
-      @define-color overlay1 #7f849c;
-      @define-color overlay0 #6c7086;
-      @define-color surface2 #585b70;
-      @define-color surface1 #45475a;
-      @define-color surface0 #313244;
-      @define-color base #1e1e2e;
-      @define-color mantle #181825;
-      @define-color crust #11111b;
+    @define-color rosewater #f5e0dc;
+@define-color flamingo #f2cdcd;
+@define-color pink #f5c2e7;
+@define-color mauve #cba6f7;
+@define-color red #f38ba8;
+@define-color maroon #eba0ac;
+@define-color peach #fab387;
+@define-color yellow #f9e2af;
+@define-color green #a6e3a1;
+@define-color teal #94e2d5;
+@define-color sky #89dceb;
+@define-color sapphire #74c7ec;
+@define-color blue #89b4fa;
+@define-color lavender #b4befe;
+@define-color text #cdd6f4;
+@define-color subtext1 #bac2de;
+@define-color subtext0 #a6adc8;
+@define-color overlay2 #9399b2;
+@define-color overlay1 #7f849c;
+@define-color overlay0 #6c7086;
+@define-color surface2 #585b70;
+@define-color surface1 #45475a;
+@define-color surface0 #313244;
+@define-color base #1e1e2e;
+@define-color mantle #181825;
+@define-color crust #11111b;
 
-      /* ===== Waybar Styling ===== */
-      * {
-        border: none;
-        border-radius: 0;
-        font-family: "JetBrainsMono Nerd Font";
-        font-size: 13px;
-        min-height: 0;
-      }
+* {
+    border: none;
+    border-radius: 0px;
+    font-family: "DroidSansM Nerd Font";
+    font-weight: bold;
+    font-size: 12px;
+    min-height: 0;
+    transition: 0.3s;
+}
 
-      window#waybar {
-        background: @base;
-        color: @text;
-      }
+window#waybar {
+    background: rgba(21, 18, 27, 0);
+    color: @text;
+}
 
-      #workspaces button {
-        padding: 0 8px;
-        color: @subtext0;
-      }
+tooltip {
+    background: @base;
+    border-radius: 10px;
+    border-width: 1.5px;
+    border-style: solid;
+    border-color: @crust;;
+    transition: 0.3s;
+}
 
-      #workspaces button.active {
-        color: @lavender;
-        background: @surface0;
-      }
+#workspaces button {
+    padding: 5px;
+    color: @overlay0;    
+    margin-right: 5px;
+}
 
-      #clock,
-      #battery,
-      #cpu,
-      #memory,
-      #temperature,
-      #network,
-      #pulseaudio,
-      #backlight,
-      #tray,
-      #idle_inhibitor {
-        padding: 0 10px;
-        background: @surface0;
-        margin: 2px;
-      }
+#workspaces button.active {
+    color: rgba(255, 85, 85, 0.85);
+}
 
-      #battery.warning { color: @yellow; }
-      #battery.critical { color: @red; }
-    '';
+#workspaces button.focused {
+    color: @text;
+    background: @maroon;
+    border-radius: 20px;
+}
+
+#workspaces button.urgent {
+    color: @crust;
+    background: @green;
+    border-radius: 20px;
+}
+
+#workspaces button:hover {
+    background: @crust;
+    color: @text;
+    border-radius: 20px;
+}
+
+#idle_inhibitor,
+#custom-power_profile,
+#custom-weather,
+#window,
+#clock,
+#battery,
+#pulseaudio,
+#network,
+#cpu,
+#memory,
+#bluetooth,
+#temperature,
+#workspaces,
+#tray,
+#backlight {
+    background: @base;
+    opacity: 0.8;
+    padding: 0px 10px;
+    margin: 0;
+    margin-top: 5px;
+    border: 1px solid @mantle;
+}
+
+#temperature {
+    border-radius: 20px 0px 0px 20px;
+}
+
+#temperature.critical {
+    color: @maroon;
+}
+
+#backlight {
+    border-radius: 20px 0px 0px 20px;
+    padding-left: 7px;
+}
+
+#tray {
+    border-radius: 20px;
+    margin-right: 5px;
+    padding: 0px 4px;
+}
+
+#workspaces {
+    background: @base;
+    border-radius: 20px;
+    margin-left: 5px;
+    padding-right: 0px;
+    padding-left: 5px;
+}
+
+#custom-power_profile {
+    color: @green;
+    border-left: 0px;
+    border-right: 0px;
+}
+
+#window {
+    border-radius: 20px;
+    margin-left: 5px;
+    margin-right: 5px;
+}
+
+#clock {
+    color: @peach;
+    border-radius: 20px;
+    margin-left: 5px;
+    border-right: 0px;
+    transition: 0.3s;
+    padding-left: 7px;
+}
+
+#network {
+    color: @yellow;
+    border-radius: 20px 0px 0px 20px;
+    border-left: 0px;
+    border-right: 0px;
+}
+
+#bluetooth {
+    color: @blue;
+    border-radius: 20px;
+    margin-right: 10px
+} 
+
+#pulseaudio {
+    color: @blue;
+    border-left: 0px;
+    border-right: 0px;
+}
+
+#pulseaudio.microphone {
+    color: #cba6f7;
+    border-left: 0px;
+    border-right: 0px;
+    border-radius: 0px 20px 20px 0px;
+    margin-right: 5px;
+    padding-right: 8px;
+}
+
+#battery {
+    color: @green;
+    border-radius: 0 20px 20px 0;
+    margin-right: 5px;
+    border-left: 0px;
+}
+
+#custom-weather {
+    border-radius: 20px;
+    border-right: 0px;
+    margin-left: 0px;
+}    '';
   };
 }
 
